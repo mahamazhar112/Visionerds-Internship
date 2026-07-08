4fe55169218141c7694cd34060edd2d71312fd58
Maha Mazhar <mahamazhar112@gmail.com>
2026-07-08 17:50:33 +0500

    Completed Week 1 Project 3 - Sentiment Analyzer

---
+++ week1/Project 3/sentiment_analyzer.py
@@ -0,0 +1,59 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("OPENROUTER_API_KEY")
+client = OpenAI(
+    api_key=api_key,
+    base_url="https://openrouter.ai/api/v1"
+)
+
+user_input = input("Enter your text: ")
+
+response = client.chat.completions.create(
+    model="openai/gpt-oss-20b:free",
+    messages=[
+        {
+            "role": "system",
+            "content": """
+You are an expert sentiment analysis assistant.
+
+Analyze the user's message and determine:
+
+- sentiment
+- emotion
+- confidence
+
+Rules:
+
+1. Return ONLY valid JSON.
+2. Do not add any explanation.
+3. Do not use Markdown.
+4. Format the JSON with proper indentation.
+5. Sentiment must be one of:
+   - Positive
+   - Negative
+   - Neutral
+6. Confidence must be a decimal value between 0 and 1.
+7. Emotion should be a single word such as:
+   Joy
+   Sadness
+   Anger
+   Fear
+   Surprise
+   Love
+   Excitement
+   Disgust
+   None
+"""
+        },
+        {
+            "role": "user",
+            "content": user_input
+        }
+    ]
+)
+print("\nSentiment Analysis:\n")
+print(response.choices[0].message.content)
